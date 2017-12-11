5c8264b85c50d840f7ff56a0f0169a990dcd48c4
Ethan <yilongshi0610@gmail.com>
2017-12-10 17:00:28 -0800

    Inital Commit

---
+++ Ethanhundreds/Ethanhundreds/Ball.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Ethanhundreds
+{
+    class Ball
+    {
+
+        public Texture2D image;
+        public Vector2 position;
+        public Color tint;
+        public Vector2 speed;
+        public float scale;
+
+
+        public bool IsDebug = false;
+
+        public int score = 0;
+
+
+        public float Radius
+        {
+            get
+            {
+                return (image.Width * scale) / 2;
+            }
+        }
+
+        public Vector2 Center
+        {
+            get
+            {
+                return position + new Vector2(Radius);
+            }
+        }
+
+
+        public Ball(Texture2D image, Vector2 position, Color tint, Vector2 speed)
+        {
+            this.image = image;
+            this.position = position;
+            this.tint = tint;
+            this.speed = speed;
+            scale = 0.05f;
+        }
+
+
+
+
+        public void Wall(Viewport screen)
+        {
+            position += speed;
+
+            if (position.Y + Height > screen.Height)
+            {
+                speed.Y = -Math.Abs(speed.Y);
+
+            }
+
+            if (position.X + Width > screen.Width)
+            {
+                speed.X = -Math.Abs(speed.X);
+            }
+
+            if (position.X < 0)
+            {
+                speed.X = Math.Abs(speed.X);
+            }
+
+            if (position.Y < 0)
+            {
+                speed.Y = Math.Abs(speed.Y);
+            }
+        }
+        
+        public bool IsColliding(Ball otherBall)
+        {
+            float sumOfRadii = Radius + otherBall.Radius;
+            float sumOfRadiiSquared = sumOfRadii * sumOfRadii;
+
+            Vector2 vectorDistance = Center - otherBall.Center;
+            float distanceSqured = vectorDistance.LengthSquared();
+
+            return sumOfRadiiSquared > distanceSqured;
+        }
+
+        public bool Collision(Ball balls)
+        {
+            Ball circle1 = this;
+            Ball circle2 = balls;
+
+            float r1 = circle1.Radius;
+            float r2 = circle2.Radius;
+
+            float circle1X = (float)(circle1.position.X + r1);
+
+            float circle1Y = (float)(circle1.position.Y + r1);
+
+            float circle2X = (float)(circle2.position.X + r2);
+
+            float circle2Y = (float)(circle2.position.Y + r2);
+
+            float distanceBetweenCirclesSquared = (float)(Math.Sqrt( Math.Pow(circle2X - circle1X, 2) + Math.Pow(circle2Y - circle1Y, 2) ));
+
+            if (r1+r2 > distanceBetweenCirclesSquared)
+            {
+                return true;
+            }
+
+            // calculate the new radius and distance if the speeds were reversed for both balls
+            // then compare the distance of this what-if scenario to the original distance
+            // if the new distance is smaller, then don't do the swap
+            // if the new distance is larger, then continue with the swap of speeds
+
+            return false;
+
+        }
+
+        public Rectangle Hitbox
+        {
+            get
+            {
+                return new Rectangle((int)position.X, (int)position.Y, (int)(Width), (int)(Height));
+            }
+        }
+
+
+        public float Width
+        {
+            get
+            {
+                return image.Width * scale;
+            }
+        }
+
+        public float Height
+        {
+            get
+            {
+                return image.Height * scale;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(image, position, null, tint, 0, Vector2.Zero, scale, SpriteEffects.None, 1f);
+
+            //spriteBatch.Draw(image, position, tint);
+            spriteBatch.Draw(image, new Rectangle((int)position.X, (int)position.Y, (int)Radius*2, (int)Radius*2), Color.Red * 0.6f);
+
+            if(IsDebug)
+            {
+                spriteBatch.Draw(Game1.Pixel, new Vector2(Hitbox.X, Hitbox.Y), Hitbox, Color.Red * 0.5f);
+            }
+
+        }
+
+    }
+}
---
+++ Ethanhundreds/Ethanhundreds/Game1.cs
@@ -0,0 +1,264 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Ethanhundreds
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Random rnd = new Random();
+
+        List<Ball> balls = new List<Ball>();
+
+
+        public static Texture2D Pixel;
+
+        int numberofball = 4;
+
+        bool run = true;
+        SpriteFont font;
+
+        int totalscore = 0;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.PreferredBackBufferHeight = 1000;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D ballimage = Content.Load<Texture2D>("ball");
+            font = Content.Load<SpriteFont>("File");
+            SpawnBalls();
+            // TODO: use this.Content to load your game content here
+
+            //Debug
+            Pixel = new Texture2D(GraphicsDevice, 1, 1);
+            Pixel.SetData(new [] { Color.White });
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        public void SpawnBalls()
+        {
+            Texture2D ballimage = Content.Load<Texture2D>("ball");
+            for (int j = 0; j < numberofball; j++)
+            {
+                //Generate a random position, and a temporary ball
+                var position = new Vector2(rnd.Next(0, 1601), rnd.Next(0, 1001));
+                var tempBall = new Ball(ballimage, position, Color.White, new Vector2(7, 7)) { IsDebug = false };
+
+                while (!isValid(tempBall))
+                {
+                    tempBall.position = new Vector2(rnd.Next(0, 1601), rnd.Next(0, 1001));
+                }
+
+                balls.Add(tempBall);            
+            }
+        }
+
+        private bool isValid(Ball tempBall)
+        {
+            foreach (var ball in balls)
+            {
+                if(ball.Collision(tempBall))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            MouseState ms = Mouse.GetState();
+            KeyboardState ks = Keyboard.GetState();
+            if (run == true)
+            {
+                for (int i = 0; i < numberofball; i++)
+                {
+                    balls[i].Wall(GraphicsDevice.Viewport);
+
+                    if (balls[i].Hitbox.Contains(ms.X, ms.Y))
+                    {
+                        balls[i].scale += 0.002f;
+                        balls[i].score++;
+                    }
+                }
+                for (int i = 0; i < numberofball; i++)
+                {
+                    for (int j = i + 1; j < numberofball; j++)
+                    {
+                        if (balls[i].Collision(balls[j]))
+                        //if (balls[i].Hitbox.Intersects(balls[j].Hitbox))
+                        {
+                            balls[i].speed.X = -balls[i].speed.X;
+                            balls[i].speed.Y = -balls[i].speed.Y;
+
+                            balls[j].speed.X = -balls[j].speed.X;
+                            balls[j].speed.Y = -balls[j].speed.Y;
+
+                            if (balls[i].Hitbox.Contains(ms.X, ms.Y))
+                            {
+                                run = false;
+                            }
+
+                            if (balls[j].Hitbox.Contains(ms.X, ms.Y))
+                            {
+                                run = false;
+                            }
+                        }
+                    }
+                }
+
+                totalscore = 0;
+                for (int i = 0; i < numberofball; i++)
+                {
+                    totalscore += balls[i].score;
+                }
+
+                if (totalscore == 100)
+                {
+                    run = false;
+                }
+            }
+            if (ks.IsKeyDown(Keys.Enter) && run == false && totalscore == 100)
+            {
+                Texture2D ballimage = Content.Load<Texture2D>("ball");
+
+                numberofball++;
+
+                balls.Add(new Ball(ballimage, new Vector2(rnd.Next(0, 1601), rnd.Next(0, 1001)), Color.White, new Vector2(7, 7)));
+
+                for (int i = 0; i < numberofball; i++)
+                {
+                    balls[i].position = new Vector2(rnd.Next(0, 1601), rnd.Next(0, 1001));
+                    balls[i].score = 0;
+                    totalscore = 0;
+                    run = true;
+                    balls[i].scale = 0.05f;
+
+                    for (int j = i + 1; j < numberofball; j++)
+                    {
+                        if (balls[i].Collision(balls[j]))
+                        {
+                            balls[i].position = new Vector2(rnd.Next(1, 1601), rnd.Next(0, 1001));
+                        }
+                    }
+                }
+
+            }
+            if (ks.IsKeyDown(Keys.Enter) && run == false && totalscore < 100)
+            {
+                Texture2D ballimage = Content.Load<Texture2D>("ball");
+
+                numberofball--;
+
+                for (int i = 0; i < numberofball; i++)
+                {
+                    balls[i].position = new Vector2(rnd.Next(0, 1601), rnd.Next(0, 1001));
+                    balls[i].score = 0;
+                    totalscore = 0;
+                    run = true;
+                    balls[i].scale = 0.05f;
+
+                    for (int j = i + 1; j < numberofball; j++)
+                    {
+                        if (balls[i].Collision(balls[j]))
+                        {
+                            balls[i].position = new Vector2(rnd.Next(1, 1601), rnd.Next(0, 1001));
+                        }
+                    }
+                }
+
+            }
+
+            base.Update(gameTime);
+            }
+        
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            for (int i = 0; i < numberofball; i++)
+            {
+                balls[i].Draw(spriteBatch);
+            }
+
+            for (int i = 0; i < numberofball; i++)
+            {
+                spriteBatch.DrawString(font, balls[i].score.ToString(), balls[i].Center, Color.White);
+            }
+
+            if( run==false && totalscore == 100)
+            {
+                spriteBatch.DrawString(font, "Next Level Hit Enter!", new Vector2(800,500), Color.Black);
+            }
+
+            if(run == false && totalscore < 100)
+            {
+                spriteBatch.DrawString(font, "Game Over!Last Level Hit Enter!", new Vector2(800, 500), Color.Black);
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
